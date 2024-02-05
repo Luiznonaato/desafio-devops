2b383256bc6cf6479d3e9d7013faab0edcf715fe
Luiz Nonato <89255917+Luiznonaato@users.noreply.github.com>
2024-02-05 17:48:19 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,25 @@
-variable "instance_type" {
-  description = "Tipo de instância EC2"
-  default     = "t2.micro"
+variable "AWS_DEFAULT_REGION" {
+  description = "Regiao"
+  type        = string
 }
 
-variable "project_name" {
-  description = "Desafio-devops"
-  default     = "Desafio-devops"
+variable "vpc_id" {
+  description = "O ID da VPC"
+  type        = string
 }
 
-variable "region" {
-  description = "A região da AWS onde os recursos serão criados"
-  default     = "us-east-1"
+variable "subnet_ids" {
+  description = "Lista de IDs das subnets para o ALB e instâncias EC2"
+  type        = list(string)
+}
+
+variable "ami_id" {
+  description = "AMI ID para as instâncias EC2"
+  type        = string
+}
+
+variable "app_dns_name" {
+  description = "O nome DNS para o registro no Route53 apontando para o ALB"
+  type        = string
+  default     = "app.bluesoft.com.br"
 }
