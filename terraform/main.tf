29f211b33d9df132eed6d748d4f180109bf8ade1
Luiz Nonato <89255917+Luiznonaato@users.noreply.github.com>
2024-02-06 15:26:19 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,11 +69,6 @@ resource "aws_ecs_cluster" "cluster" {
   name = "seu-cluster-ecs"  # Substitua pelo nome desejado do cluster ECS
 }
 
-resource "aws_ecs_service" "meu_servico_ecs" {
-  name    = "meu-servico-ecs"
-  cluster = aws_ecs_cluster.cluster.id
-}
-
 output "ecs_service_name" {
   value = aws_ecs_service.meu_servico_ecs.name
 }
