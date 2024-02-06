fbbc5b9a9f53d1bcafe2b66e173fb2f8183d342d
Luiz Nonato <89255917+Luiznonaato@users.noreply.github.com>
2024-02-06 06:55:26 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -97,11 +97,11 @@ resource "aws_autoscaling_group" "ecs_asg" {
 }
 
 # Registro DNS no Route53
-resource "aws_route53_record" "dns" {
+/*resource "aws_route53_record" "dns" {
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = var.app_dns_name
   type    = "A"
-
+*/
   alias {
     name                   = aws_lb.alb.dns_name
     zone_id                = aws_lb.alb.zone_id
