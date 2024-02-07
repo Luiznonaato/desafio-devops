bf391917a8ed95c493e1a2445c2b8309e064fa0e
luiz.anribeiro <luiz.anribeiro@gmail.com>
2024-02-06 23:56:42 -0300

    d

--- terraform/modules/ecr/variables.tf
+++ terraform/modules/ecr/variables.tf
@@ -1,9 +1,7 @@
 variable "ecr_repository_name" {
   type    = string
-  default = file("${path.modules}/../variables.tf")
 }
 
 variable "ami_id" {
   type    = string
-  default = file("${path.modules}/../variables.tf")
 }
--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -1,4 +1,3 @@
 variable "ami_id" {
   type    = string
-  default = file("${path.modules}/../variables.tf")
 }
