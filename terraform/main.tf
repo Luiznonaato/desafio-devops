16528370f8a1d7c3e4a1926246bbabedf4146ba3
luiz.anribeiro <luiz.anribeiro@gmail.com>
2024-02-06 23:17:14 -0300

    d

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,28 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./modules/vpc/main.tf"
   // Parâmetros para o módulo VPC
 }
 
 module "security_group" {
-  source = "./modules/security_group"
+  source = "./modules/security_group/main.tf"
   // Parâmetros para o módulo Security Group
 }
 
 module "ecr" {
-  source = "./modules/ecr"
+  source = "./modules/ecr/main.tf"
   // Parâmetros para o módulo ECR
 }
 
 module "ecs" {
-  source = "./modules/ecs"
+  source = "./modules/ecs/main.tf"
   // Parâmetros para o módulo ECS
 }
 module "route" {
-  source = "./modules/route"
+  source = "./modules/route/main.tf"
   // Parâmetros para o módulo ECS
 }
 module "alb" {
-  source              = "./modules/alb"
+  source              = "./modules/alb/main.tf"
   alb_name            = "meu-alb"
   security_groups     = [module.security_group.sg_id]
   subnets             = module.vpc.subnets
