9e552e86aa664c70e2b9c7791e33c9f9e07096cb
luiz.anribeiro <luiz.anribeiro@gmail.com>
2024-02-07 09:57:01 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,26 +12,15 @@ module "ecr" {
 }
 module "ecs" {
   source = "./modules/ecs"
-  ami_id = "ami-0277155c3f0ab2930"
+  ami_id = ami_id.default
 }
 module "route" {
   source = "./modules/route"
 }
 module "alb" {
-  source              = "./modules/alb/"
-  /*alb_name            = "meu-alb"
-  security_groups     = [module.security_group.sg_id]
-  subnets             = module.vpc.subnets
-  target_group_name   = "ecs-target-group"
-  target_group_port   = 8080
-  vpc_id              = module.vpc.vpc_id
-  health_check_path   = "/actuator/health"
-  health_check_matcher = "200"
-  health_check_interval = 30
-  health_check_timeout = 5
-  health_check_healthy_threshold = 2
-  health_check_unhealthy_threshold = 2
-  listener_port       = 8080*/
+  source = "./modules/alb/"
+  security_groups = [module.security_group.security_group_id]
+  subnets         = [module.subnet.subnet_id] // Isso supõe que você tenha um módulo para subnet com um output `subnet_id`
 }
 
 
