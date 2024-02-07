c08f8c0a12605b3870b49abec660640f156c7bc3
luiz.anribeiro <luiz.anribeiro@gmail.com>
2024-02-07 09:52:51 -0300

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,10 +1,20 @@
 # Application Load Balancer
+variable "security_groups" {
+  description = "List of security group IDs to associate with the ALB"
+  type        = list(string)
+}
+
+variable "subnets" {
+  description = "List of subnet IDs to associate with the ALB"
+  type        = list(string)
+}
+
 resource "aws_lb" "alb" {
   name               = "meu-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.sg.id]
-  subnets            = [aws_subnet.subnet_id.id]
+  security_groups    = var.security_groups.security_group_id
+  subnets            = var.subnets.subnet_id
 }
 
 resource "aws_lb_listener" "front_end" {
@@ -17,3 +27,4 @@ resource "aws_lb_listener" "front_end" {
     target_group_arn = aws_lb_target_group.ecs_target_group.arn
   }
 }
+
