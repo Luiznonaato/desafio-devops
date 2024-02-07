143f5f7474e5038870869d96eb0b8c63e1552cb1
luiz.anribeiro <luiz.anribeiro@gmail.com>
2024-02-07 09:44:30 -0300

    d

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ module "security_group" {
 }
 module "ecr" {
   source = "./modules/ecr"
-  //ami_id = "ami-0277155c3f0ab2930"
+  ami_id = "ami-0277155c3f0ab2930"
 }
 module "ecs" {
   source = "./modules/ecs"
